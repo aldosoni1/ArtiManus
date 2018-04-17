a3bf6b69cb07254bce472515c68e6678475d5bc6
Aldo <aldotesa96@gmail.com>
2018-04-17 18:30:58 -0500

    Pantalla de login y toolbar

--- app/src/main/java/com/example/aldo/artimanus/views/LoginActivity.java
+++ app/src/main/java/com/example/aldo/artimanus/views/LoginActivity.java
@@ -17,14 +17,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
 }
