202a52c27ddc104990edb216fc850e7e28350543
Aldo <aldotesa96@gmail.com>
2018-04-18 12:30:23 -0500

    toolbar

---
+++ app/src/main/java/com/example/aldo/artimanus/views/SubirProductoActivity.java
@@ -0,0 +1,15 @@
+package com.example.aldo.artimanus.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aldo.artimanus.R;
+
+public class SubirProductoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subir_producto);
+    }
+}
