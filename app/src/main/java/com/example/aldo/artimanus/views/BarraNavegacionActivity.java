c9b9e91a08983b44088c0f1ce6214b4d3eddd762
Aldo <aldotesa96@gmail.com>
2018-04-23 18:26:13 -0500

    Barra de navegación

---
+++ app/src/main/java/com/example/aldo/artimanus/views/BarraNavegacionActivity.java
@@ -0,0 +1,15 @@
+package com.example.aldo.artimanus.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aldo.artimanus.R;
+
+public class BarraNavegacionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barra_navegacion);
+    }
+}
