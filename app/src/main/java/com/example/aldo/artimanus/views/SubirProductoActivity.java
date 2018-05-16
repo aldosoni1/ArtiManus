b0b9b4c08701e214c19192abaff89ff4abbd2947
JOSSELYN TSASNAI PEREZ LEON <joss.leon@comunidad.unam.mx>
2018-05-15 21:59:52 -0500

    modifiqué el xml y el activity de Subir Producto

--- app/src/main/java/com/example/aldo/artimanus/views/SubirProductoActivity.java
+++ app/src/main/java/com/example/aldo/artimanus/views/SubirProductoActivity.java
@@ -1,15 +1,44 @@
 package com.example.aldo.artimanus.views;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 
 import com.example.aldo.artimanus.R;
 
 public class SubirProductoActivity extends AppCompatActivity {
+    ImageView imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subir_producto);
+
+        imagen = findViewById(R.id.ImagenId);
+    }
+
+    public void onclick(View view) {
+        cargarImagen();
+    }
+
+    private void cargarImagen() {
+        Intent intent = new Intent (Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent, "seleccione la aplicación"), 10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Uri path = data.getData();
+            imagen.setImageURI(path);
+        }
     }
 }
